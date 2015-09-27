c614a487a6934cb449abdace121b93b382b4657e
Arjun Venkatraman <arjun@mojolab.org>
2015-09-27 21:42:26 +0530

    Fixed path

--- setupasterisk.sh
+++ setupasterisk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp -arv /etx/asterisk /etc/asterisk.orig
+sudo cp -arv /etc/asterisk /etc/asterisk.orig
 sudo cp /opt/mojobol/conf/extensions.conf /etc/asterisk/extensions.conf
 sudo cp /opt/mojobol/conf/sip.conf /etc/asterisk/sip.conf
 sudo /etc/init.d/asterisk restart
