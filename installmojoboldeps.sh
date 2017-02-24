32fd808fadfb1d6837204a7856aa58162cf32fc7
Arjun Venkatraman <arjun@mojolab.org>
2017-02-24 14:37:02 +0000

    Added installer dep

---
+++ installmojoboldeps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install asterisk python-setuptools git espeak wget lame mysql-server gcc g++ php5-mysql php5 apache2 python python-mysqldb geany libsox-fmt-mp3 sox ipython
+
+sudo easy_install pyyaml pytz xlrd
