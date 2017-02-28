fe012c084bd425748e08ca0ae41e8413b0e5ef3b
Arjun Venkatraman <arjun@mojolab.org>
2017-02-28 13:53:51 +0530

    Updated

--- installmojoboldeps.sh
+++ installmojoboldeps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install asterisk python-setuptools git espeak wget lame mysql-server gcc g++ php5-mysql php5 apache2 python python-mysqldb geany libsox-fmt-mp3 sox ipython
-
+sudo apt-get install asterisk python-setuptools git espeak wget lame mysql-server gcc g++ php5-mysql php5 apache2 python python-mysqldb geany libsox-fmt-mp3 sox ipython zip
 sudo easy_install pyyaml pytz xlrd
+
+cd /opt
+git clone https://github.com/mojolab/livingdata.git
+git clone https://github.com/mojolab/mojomailman
