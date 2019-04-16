8203e3d834c7f785ce2046d0d205ae5d593eb330
Arjun Venkatraman <mojoarjun@gmail.com>
2019-04-16 16:04:19 +0530

    Committing

--- bin/mojobol-svr.py
+++ bin/mojobol-svr.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from mojoasteriskplayer import *
+from datetime import *
+from mojobol import *
 import sys
 sys.path.append("/opt/mojobol/libs")
-from mojobol import *
-from datetime import *
-from mojoasteriskplayer import *
-if __name__=="__main__":
-	env=read_agi_environment()
-	ms=MojoBolResponder("/opt/voh/voh.conf")
-	call=MojoBolCall(ms,env)
-	p=call.responder.parse_workflow(call)
-	call.endcall()
-	call.compresscallfile()
+if __name__ == "__main__":
+    env = read_agi_environment()
+    ms = MojoBolResponder("/opt/voh/voh.conf")
+    call = MojoBolCall(ms, env)
+    p = call.responder.parse_workflow(call)
+    call.endcall()
+    call.compresscallfile()
