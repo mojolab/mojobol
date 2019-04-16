5c24b481dd891e15780eade1d21f21c4b1d9ca21
Arjun Venkatraman <mojoarjun@gmail.com>
2019-04-16 16:08:47 +0530

    Updated to fix #9

--- bin/mojobol-svr.py
+++ bin/mojobol-svr.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append("/opt/mojobol/libs")
 from mojoasteriskplayer import *
 from datetime import *
 from mojobol import *
-import sys
-sys.path.append("/opt/mojobol/libs")
 if __name__ == "__main__":
     env = read_agi_environment()
     ms = MojoBolResponder("/opt/voh/voh.conf")
