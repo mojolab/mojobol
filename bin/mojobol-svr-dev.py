8707b4f098e7539110ad0d563ea6fc2598afe7f9
Arjun Venkatraman <mojoarjun@gmail.com>
2021-06-05 21:52:16 +0530

    Updated

---
+++ bin/mojobol-svr-dev.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append("/opt/mojobol/libs")
+from mojoasteriskplayer import *
+from datetime import *
+from mojobol import *
+if __name__ == "__main__":
+    env = read_agi_environment()
+    ms = MojoBolResponder("/opt/shwaasivr/shwaasivr.conf")
+    call = MojoBolCall(ms, env)
+    p = call.responder.parse_workflow(call)
+    call.endcall()
+    call.compresscallfile()
+    call.updatedf()
