da8c9c19eed8728dd939eac5a9e6d4052505a49f
Arjun Venkatraman <mojoarjun@gmail.com>
2021-06-05 23:32:21 +0530

    Updated

--- bin/mojobol-svr.py
+++ bin/mojobol-svr.py
@@ -7,7 +7,7 @@ from datetime import *
 from mojobol import *
 if __name__ == "__main__":
     env = read_agi_environment()
-    ms = MojoBolResponder("/opt/voh/voh.conf")
+    ms = MojoBolResponder("/opt/shwaasivr/voh.conf")
     call = MojoBolCall(ms, env)
     p = call.responder.parse_workflow(call)
     call.endcall()
